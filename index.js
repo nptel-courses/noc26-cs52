3bffbf9c9229c26d6227b39c666549831c21b065
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-04-25 11:15:32 +0530

    github pages: updated .html for WEEK 01: Assignment 01 - Problem 01

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ window.htmlFiles = [
         "path": "sol/practice/WEEK 01/P01/PP01.html"
     },
     {
+        "path": "sol/practice/WEEK 01/P02.html"
+    },
+    {
+        "path": "sol/practice/WEEK 02/P01.html"
+    },
+    {
         "path": "sol/sessions/WEEK 01/L01.html"
     }
 ];
